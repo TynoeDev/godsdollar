755d3f511f9e3cd9b07eb7ac2bf0498cb4d48929
Tinotenda Chigwida <chigwidath@gmail.com>
2025-04-25 11:42:50 +0200

    run2

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-// vite.config.ts
+vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/godsdollar/", // YOUR REPO NAME HERE
+  base: "/godsdollar", // YOUR REPO NAME HERE
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
